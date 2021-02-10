db56a9271e9ae310d6b7a5f83d7ce486fe8f605a
Iván <Ivantiz45@gmail.com>
2021-02-10 21:22:41 +0100

    Head quitado

--- ejercicios_react/pages/hireme.js
+++ ejercicios_react/pages/hireme.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Head from 'next/head';
+import Leyout from './compos/Layout'
 import Link from 'next/link';
-import Layout from '/compos/Layout'
+import Layout from './compos/Layout';
 const hireme = () => (
-    <Layout>
-        <Head>
-        <title>Usoib</title>
-        <link rel="icon" href="/Images/usoib.ico"></link>
-        </Head>
-
-        <h1>Hire Me</h1>
+    <Layout title="Hire Me">
         <p>
             You can hire me at {""}
             <a href="mailto:ivantiz45@gmail.com"> ivantiz45@gmail.com</a>
