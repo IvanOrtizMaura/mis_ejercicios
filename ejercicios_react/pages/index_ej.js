d970a0e94008d17bd9a713c5c1d3efc661b0b362
Iván Ortiz Maura <ivantiz45@gmail.com>
2021-02-10 14:21:53 +0100

    Head y links

--- ejercicios_react/pages/index_ej.js
+++ ejercicios_react/pages/index_ej.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
 const index = () => ( 
     <div>
-        <h1>Home pages</h1>
-            welcome to the home pages
+        <Head>
+        <title>Usoib</title>
+        <link rel="icon" href="Images/usoib.ico"/>
+        </Head>
+        <div>
+            <h1>Home pages</h1>
+            <Link href="/about">
+            <a>Go to about</a>
+            </Link>
+            <p>Welcome to the home pages</p>
+        </div>
     </div>
+    
+   
 );
 
 export default index;
