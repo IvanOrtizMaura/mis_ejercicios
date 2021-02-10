1e962d6b4bbbf5c2a652aa30ca60b1781950e6a9
Iván Ortiz Maura <ivantiz45@gmail.com>
2021-02-10 13:40:15 +0100

    Inserta Imagenes

---
+++ ejercicios_react/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const about = () => (
+    <div>
+        <h1>About</h1>
+        <a href=" n">go to home</a>
+        <p>A JavaScript programer</p>
+        <img src="Images/JavaScript-logo.png" alt="JavaScript" width="5%"/>
+    </div>
+);
+export default about 
