26e5c05b2cf58af3d34b72134b44ebdc95b08a50
Iván <Ivantiz45@gmail.com>
2021-02-10 21:22:27 +0100

    Estructura con head

--- ejercicios_react/pages/compos/Layout.js
+++ ejercicios_react/pages/compos/Layout.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const layout = ({children, title}) => (
  <div>
+     <Head>
+         <title>Usoib</title>
+         <link rel="icon" href="/Images/usoib.ico"></link>
+     </Head>
      <header>
         <Link href="/index_ej"><a>Home</a></Link>
         <Link href="/about"><a>About</a></Link>
         <Link href="/hireme"><a>Hire Me</a></Link>
      </header>
-
+     <h1>{title}</h1>
 
     {children}
-    <h1>{title}</h1>
+    
 
      <footer>
         footer
