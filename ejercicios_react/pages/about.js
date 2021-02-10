5d9744ac70f57f6f86152ac6aeb74654cc01569f
Iván Ortiz Maura <ivantiz45@gmail.com>
2021-02-10 14:21:40 +0100

    Link bien hecho

--- ejercicios_react/pages/about.js
+++ ejercicios_react/pages/about.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
 const about = () => (
     <div>
+        <Head>
+        <title>Usoib</title>
+        <link rel="icon" href="Images/usoib.ico"/>
+        </Head>
         <h1>About</h1>
-        <a href=" n">go to home</a>
+        <Link href="/index_ej">
+        <a>Go to Home Pages</a>
+        </Link>
         <p>A JavaScript programer</p>
         <img src="Images/JavaScript-logo.png" alt="JavaScript" width="5%"/>
     </div>
