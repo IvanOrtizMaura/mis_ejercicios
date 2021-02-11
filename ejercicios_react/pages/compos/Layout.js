0cc3eead019a5bc51c26d2579f89e3f24cf920be
Iván Ortiz Maura <ivantiz45@gmail.com>
2021-02-11 15:12:56 +0100

    Estilos

--- ejercicios_react/pages/compos/Layout.js
+++ ejercicios_react/pages/compos/Layout.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Head from 'next/head'
 
 const layout = ({children, title}) => (
- <div>
+ <div className="root">
      <Head>
          <title>Usoib</title>
          <link rel="icon" href="/Images/usoib.ico"></link>
@@ -19,8 +19,52 @@ const layout = ({children, title}) => (
     
 
      <footer>
-        footer
+        &copy;{new Date().getFullYear()}
      </footer>
+
+     <style jsx>{`
+         .root{
+            display: flex;
+            justify-content: center; 
+            align-items: center;
+            flex-direction: column;
+        }
+
+        header{
+            width: 100%;
+            display: flex;
+            justify-content: space-around;
+            padding: 1em;
+            font-size:1.2rem;
+            background: indigo;
+        }
+
+        header a{
+            color: darkgrey;
+            text-decoration: none;
+        }
+
+        header a:hover{
+            font-weight: bold;
+            color: lightgrey;
+        }
+
+        footer{
+            
+            padding; 1em;
+            color: drakgrey;
+        }
+     `}
+     </style>
+
+     <style global jsx>{`
+        body {
+            margin:0;
+            font-size: 110%;
+            background: #f0f0f0;
+        }
+     `}
+     </style>
  </div>
 );
 export default layout;
